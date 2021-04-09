3ebee0ca728aa0c0ba20fdb922146d3517e085ee
alfathlathif19 <79137289+alfathlathif19@users.noreply.github.com>
2021-04-09 23:10:13 +0700

    Add files via upload

---
+++ VendingMachine/VendingMachine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uangPembelian;
+            inputMoney _inputMoney = new inputMoney();
+            Money money = new Money();
+            while (money.GetValue() == 0)
+            {
+                Console.WriteLine("Masukan uang anda dengan nominal 10, 20, 50, atau 100");
+                uangPembelian = Convert.ToInt32(Console.ReadLine());
+                _inputMoney.totalUang(uangPembelian, money);
+            }
+
+            inputUser pembeli = new inputUser();
+            pembeli.menu(money);
+
+
+            Console.WriteLine();
+            Console.Write("Sisa uang anda: {0}", money.GetValue());
+        }
+    }
+}
